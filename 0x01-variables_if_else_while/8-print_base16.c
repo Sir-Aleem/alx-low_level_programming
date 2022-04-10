876bd741e330fc3cbe9c93ec1e5ed5e0acd88eab
Sir-Aleem <aleemadesina@gmail.com>
2022-04-10 09:47:28 -0700

    Task 7 base 10 and hexadecimal soup

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int i;
+int j;
+for (i = 48; i <= 57; i++)
+{
+putchar(i);
+}
+for (j = 65; j <= 70; j++)
+{
+putchar(j);
+}
+putchar('\n');
+return (0);
+}
