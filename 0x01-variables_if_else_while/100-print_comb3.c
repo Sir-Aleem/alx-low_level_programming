84e8556713de3ebec7b495f1a073ccd3c02920ae
Sir-Aleem <aleemadesina@gmail.com>
2022-04-11 04:06:59 -0700

    Task 10.0 01 ... 89 for_loop

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,7 +10,7 @@ int no1;
 int no2;
 for (no1 = 0; no1 < 9; no1++)
 {
-for (no2 += no1; no2 < 10; no2++)
+for (no2 = no1 + 1; no2 < 10; no2++)
 {
 putchar((no1 % 10) + '0');
 putchar((no2 % 10) + '0');
