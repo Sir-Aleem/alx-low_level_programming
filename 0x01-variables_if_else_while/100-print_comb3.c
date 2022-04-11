a8aa46e7750b43b5f68a89ad79790b8d3583ed7a
Sir-Aleem <aleemadesina@gmail.com>
2022-04-11 04:03:19 -0700

    Task 10.0 01 ... 89 for_loop

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+int no1;
+int no2;
+for (no1 = 0; no1 < 9; no1++)
+{
+for (no2 += no1; no2 < 10; no2++)
+{
+putchar((no1 % 10) + '0');
+putchar((no2 % 10) + '0');
+if (no1 == 8 && no2 == 9)
+{
+break;
+}
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
+
