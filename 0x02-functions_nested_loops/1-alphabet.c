b90809f708b02e998f4cbb9f9883dfb07d42d4b7
Sir-Aleem <aleemadesina@gmail.com>
2022-04-12 19:09:25 -0700

    Task 1, printalphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - Check code
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+print_alphabet();
+return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
 int _putchar(char ch);
+void print_alphabet(void);
 #endif
