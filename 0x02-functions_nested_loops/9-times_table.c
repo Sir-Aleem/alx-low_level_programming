737688d6b03320c9ff558d14fda13639efe29ba5
Sir-Aleem <aleemadesina@gmail.com>
2022-04-13 10:03:38 -0700

    Task 9, Times Table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ *times_table - Entry point
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+int i = 0, j;
+while (i < 10)
+{
+j = 0;
+while (j < 10)
+{
+int k = i * j;
+if (k == 0)
+{
+_putchar(k + '0');
+}
+else if (k <= 9)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(k + '0');
+}
+else if (k > 9)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((k / 10) + '0');
+_putchar((k % 10) + '0');
+}
+j++;
+}
+i++;
+}
+_putchar('\n');
+}
