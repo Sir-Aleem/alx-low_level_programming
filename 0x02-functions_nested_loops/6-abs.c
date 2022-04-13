953d2987fe0ef5c36b645cccdeeeb529a03af334
Sir-Aleem <aleemadesina@gmail.com>
2022-04-13 06:09:42 -0700

    Task 6, Absoloute Val

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ * @n: parameter to print
+ *
+ * Return: int n or n * -1
+ */
+int _abs(int n)
+{
+if (n >= 0)
+return (n);
+else
+return (n * -1);
+}
