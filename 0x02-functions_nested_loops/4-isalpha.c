212f2d58766e1305e384c15bb13c4168e0932214
Sir-Aleem <aleemadesina@gmail.com>
2022-04-13 04:21:47 -0700

    Task 4, isAlpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isalpha - Entry point of c
+ *@c: int to print
+ *
+ * Return: 1 or 0
+ */
+int _isalpha(int c)
+{
+if (c >= 65 && c <= 80 && c >= 97 && c <= 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
