a8c1739b0d12ac26afe53ed3ff2789342b90e17a
Sir-Aleem <aleemadesina@gmail.com>
2022-04-14 06:34:23 -0700

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_line - Entry point
+ * @n: int to print
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+}
