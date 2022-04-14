8fd08c82e013f1a7138841422ae9c588778551ec
Sir-Aleem <aleemadesina@gmail.com>
2022-04-13 17:59:23 -0700

    TAsk 11, print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - Entry point
+ * @n: int to print
+ *
+ * Return: void
+ */
+void print_to_98(int n)
+{
+if (n < 98)
+{
+int i;
+for (i = n; i <= 98; i++)
+{
+printf("%d", i);
+if (i == 98)
+break;
+putchar(',');
+putchar(' ');
+}
+}
+if (n >= 98)
+{
+int j;
+for (j = n; j >= 98; j--)
+{
+printf("%d", j);
+if (j == 98)
+break;
+putchar(',');
+putchar(' ');
+}
+}
+}
