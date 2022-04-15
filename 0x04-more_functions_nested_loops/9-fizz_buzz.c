850044f67bfb44a89a2365769f7701cd7e1dd86b
Sir-Aleem <aleemadesina@gmail.com>
2022-04-15 14:50:19 -0700

    Task 9, fuzz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,17 +11,22 @@ for (i = 1; i < 101; i++)
 {
 if (i == 1)
 {
-printf("%d", i);
+printf(" %d", i);
 }
-else if (i > 1)
+if ((i % 3) == 0 && (i % 5) != 0)
 {
-if ((i % 3) == 0)
 printf(" Fizz");
-else if ((i % 5) == 0)
+}
+else if ((i % 5) == 0 && (i % 3) != 0)
+{
 printf(" Buzz");
-else if ((i % 3) == 0 || (i % 5) == 0)
+}
+else if ((i % 3) == 0 && (i % 5) == 0)
+{
 printf(" FizzBuzz");
+}
 else
+{
 printf(" %d", i);
 }
 }
