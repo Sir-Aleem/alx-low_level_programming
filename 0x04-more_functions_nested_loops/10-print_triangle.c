20c3f3d46b887d7b009d2fff394ebd5a28a03064
Sir-Aleem <aleemadesina@gmail.com>
2022-04-15 12:09:14 -0700

    Task !0 print triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_triangle - Entry point
- * size: int to print
+ * @size: int to print
  *
  * Return: void
  */
@@ -10,15 +10,15 @@ void print_triangle(int size)
 if (size > 0)
 {
 int i;
-for (i = 1; i <= size; i++)
+for (i = 0; i < size; i++)
 {
 int j;
-for (j = 1; j < (size - i); j--)
+for (j = 0; j < ((size - 1) - i); j--)
 {
 _putchar(' ');
 }
 int k;
-for (k = 0; k < i; k++)
+for (k = 0; k <= i; k++)
 {
 _putchar('#');
 }
