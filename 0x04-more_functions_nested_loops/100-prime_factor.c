d7b96f9f2c734944bcdb72836ccedac96ca55ec1
Sir-Aleem <aleemadesina@gmail.com>
2022-04-15 17:15:47 -0700

    Task 10.1, prime

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success
+ */
+int main(void)
+{
+unsigned long n, i, max;
+n = 612852475143;
+max = 1;
+for (i = 3; i <= n; i += 2)
+{
+while ((n % i) == 0)
+{
+max = n;
+n = n / i;
+}
+}
+printf("%lu\n", max);
+return (0);
+}
