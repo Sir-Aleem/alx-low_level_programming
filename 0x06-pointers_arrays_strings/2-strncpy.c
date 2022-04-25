34e835a51ee5ebe3436a9f39d6f84bb4afeb97a9
Sir-Aleem <aleemadesina@gmail.com>
2022-04-24 20:32:29 -0700

    My string and Copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* _strncpy - Entry point
+* @dest: char to print
+* @src: char toconcatenate
+* @n: int to concatenate
+* Return: char
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+int j;
+i = 0;
+while (dest[i])
+{
+i++;
+}
+for (j = 0; j < n && src[j] != '\0'; j++)
+{
+dest[j] = src[j];
+}
+return (dest);
+}
