1cd3e229dc9b9c0bbdc40e0874f24cab14bba1c9
Sir-Aleem <aleemadesina@gmail.com>
2022-04-27 05:02:12 -0700

    My string comparism

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcmp - Entry point
+ * @s1: char
+ * @s2: char 
+ *
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+int i;
+for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+{
+if (s1[i] != s2[i])
+{
+return (s1[i] - s2[i]);
+break;
+}
+}
+return (0);
+}
