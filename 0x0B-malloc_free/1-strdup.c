44c5eabd14deb696807f64bb172fd8e1a8209a8f
Sir-Aleem <aleemadesina@gmail.com>
2022-05-08 08:55:53 -0700

    My string duplication

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,22 +10,19 @@ char *_strdup(char *str)
 int i = 0, j;
 char *s;
 if (str == NULL)
-{
 return (NULL);
-}
+
 while (str[i] != '\0')
-{
 i++;
-}
+
 s = malloc(sizeof(char) * (i + 1));
+
 if (s == NULL)
-{
 return (NULL);
-}
+
 for (j = 0; j < i; j++)
-{
 s[j] = str[j];
 s[j] = '\0';
-}
+
 return (s);
 }
