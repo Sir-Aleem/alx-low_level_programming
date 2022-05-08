1bb8187063c6b0302d66814bbbeff0b36412cc59
Sir-Aleem <aleemadesina@gmail.com>
2022-05-08 08:51:34 -0700

    The String concatenation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,35 +11,29 @@ char *str_concat(char *s1, char *s2)
 char *strconcat;
 int i = 0, j = 0, k, l, m;
 if (s1 == NULL)
-{
 s1 = "";
-}
+
 if (s2 == NULL)
-{
 s2 = "";
-}
+
 while (s1[i] != '\0')
-{
 i++;
-}
+
 while (s2[j] != '\0')
-{
 j++;
-}
+
 k = i + j + 1;
 strconcat = malloc(sizeof(char) * k);
+
 if (strconcat == NULL)
-{
 return (NULL);
-}
+
 for (l = 0; l < i; l++)
-{
 strconcat[l] = s1[l];
-}
+
 for (m = 0; l < k && m < j; l++, m++)
-{
 strconcat[l] = s2[m];
 strconcat[l] = '\0';
-}
+
 return (strconcat);
 }
