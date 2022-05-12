5d02c3c388cc2f1d3bc5e4f8d15fed405f6e23f9
Sir-Aleem <aleemadesina@gmail.com>
2022-05-12 00:46:19 -0700

    Main macro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print the name of the file this program is compiled from
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
