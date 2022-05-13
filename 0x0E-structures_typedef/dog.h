ea72d2105829ba2dd2984f26113b9206e9aef1ae
Sir-Aleem <aleemadesina@gmail.com>
2022-05-13 03:40:46 -0700

    Header dog and struct definition

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - basic structure of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: the owner of the dog.
+ *
+ * Description: User-defined data tzpe for a dog.
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
