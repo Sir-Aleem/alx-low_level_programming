07abee9680bd5236a72b947f0b421b40cdff7a22
Sir-Aleem <aleemadesina@gmail.com>
2022-05-16 10:00:16 -0700

    My Sum

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,6 +1,6 @@
 #include "function_pointers.h"
 /*
- * array_iterator - executes a function given as a parameter
+ * array_iterator - a function given as a parameter
  * on each element of an array.
  * @array: an array of integers
  * @size: the size of array
@@ -9,7 +9,7 @@
  *
  * Return: nothing
  */
-void array_iterator(int *array, int size, void (*action)(int))
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
 int i;
 if (!array || !size || !action)
---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - sum all the argument parameter
+ * @n: int, index of the arg_param
+ *
+ * Return: int sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+if (n == 0)
+return (0);
+
+va_list ap;
+va_start(ap, n);
+
+unsigned int i;
+int sum = 0;
+
+for (i = 0; i < n; i++)
+sum += va_arg(ap, int);
+
+va_end(ap);
+return (sum);
+}
+
